04d2fcc119e48e6d628793f02cb2902a6252dc52
Jaquelin-code <20652@virtual.utsc.edu.mx>
2024-09-20 23:43:48 -0600

    Primer Commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#productosTable').DataTable({
+        "language": {
+            "search": "Buscar:",
+            "lengthMenu": "Mostrar _MENU_ registros por página",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtrado de _MAX_ registros en total)"
+        }
+    });
+});
